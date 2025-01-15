32c4cc7498e8b254f087eed3d9936bac88d44321
LuaV16 <lua.vargas.ulloa@gmail.com>
2025-01-15 19:51:46 +0100

    Create function getComputerChoice
    Created the function that returns rock, paper or scissors at random

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+/* 
+Create a random int number between 1 and 3
+Create an If condition
+If the number is 1, return 'rock'
+If the number is 2, return 'paper'
+If the number is 3, return 'scissors'
+*/
+
+
+function getComputerChoice(){
+    let number = Math.floor(Math.random() * 3 + 1);
+    if(number === 1){
+        return('Rock');
+    }
+    else if(number === 2){
+        return('Paper');
+    }
+    else if(number === 3){
+        return('Scissors');
+    }
+}
+
+
+console.log(getComputerChoice());
