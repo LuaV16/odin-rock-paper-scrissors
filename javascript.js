7b0443c2c35180b6c70fb0bbe7f21772946971aa
LuaV16 <lua.vargas.ulloa@gmail.com>
2025-01-16 20:08:04 +0100

    Add playGame function

--- javascript.js
+++ javascript.js
@@ -31,13 +31,14 @@ If the user writes something else, return a warning
 
 function getHumanChoice(){
     let answer = prompt('Rock, paper, scissors?');
-    if(answer === 'Rock' || answer === 'rock'){
+    answer = answer.toLowerCase();
+    if(answer === 'rock'){
         return('Rock');
     }
-    else if(answer === 'Paper' || answer === 'paper'){
+    else if(answer === 'paper'){
         return('Paper');
     }
-    else if(answer === 'Scissors' || answer === 'scissors'){
+    else if(answer === 'scissors'){
         return('Scissors');
     }
     else{
@@ -45,5 +46,84 @@ function getHumanChoice(){
     }
 }
 
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === 'Rock'){
+        if(computerChoice === 'Rock'){
+            console.log('Its a tie!');
+            return('Tie');
+        }
+        else if(computerChoice === 'Paper'){
+            console.log('You lose! Paper beats Rock');
+            return('Computer Wins');
+        }
+        else if(computerChoice === 'Scissors'){
+            console.log('You win! Rock beats Scissors');
+            return('Human Wins');
+        }
+    }
+    else if(humanChoice === 'Paper'){
+        if(computerChoice === 'Rock'){
+            console.log('You win! Paper beats Rock');
+            return('Human Wins');
+        }
+        else if(computerChoice === 'Paper'){
+            console.log('Its a tie!');
+            return('Tie');
+        }
+        else if(computerChoice === 'Scissors'){
+            console.log('You lose! Scissors beats Paper');
+            return('Computer Wins');
+        }
+    }
+    else if(humanChoice === 'Scissors'){
+        if(computerChoice === 'Rock'){
+            console.log('You lose! Rock beats Scissors');
+            return('Computer Wins');
+        }
+        else if(computerChoice === 'Paper'){
+            console.log('You win! Scissors beats Paper');
+            return('Human Wins');
+        }
+        else if(computerChoice === 'Scissors'){
+            console.log('Its a tie!');
+            return('Tie');
+        }
+    }
+}
 
-console.log(getHumanChoice());
+function playGame(){
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for(let i = 1; i < 6; i++){
+
+        console.log('Round ' + i);
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        let result = playRound(humanChoice, computerChoice);
+        if (result === 'Computer Wins'){
+            computerScore =+ 1
+        }
+        else if (result === 'Human Wins'){
+            humanScore =+ 1
+        }
+    }
+
+    if (humanScore > computerScore){
+        console.log('You win!')
+    }
+    else if (humanScore < computerScore){
+        console.log('You lose')
+    }
+    else  {
+        console.log('Its a tie!');
+    }
+}
+
+
+
+
+
+
+playGame();
